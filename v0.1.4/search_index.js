78e9742fa98ac580bc77905ebb29608c732e5be6
Documenter.jl <documenter@juliadocs.github.io>
2022-06-18 13:01:39 +0000

    build based on 72a828b

---
+++ v0.1.4/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = FastaLoader","category":"page"},{"location":"#FastaLoader","page":"Home","title":"FastaLoader","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for FastaLoader.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [FastaLoader]","category":"page"}]
+}
