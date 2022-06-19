5d6b0f505c85a2d6c2e7e3da9c3f017ed0d518a6
kchu25 <skchu@wustl.edu>
2022-06-19 15:05:58 -0500

    add exports

--- src/FastaLoader.jl
+++ src/FastaLoader.jl
@@ -2,7 +2,13 @@ module FastaLoader
 
 using SeqShuffle, CUDA, StatsBase, Random
 
-export FASTA_DNA, make_FASTA_DNA_w_splits
+export FASTA_DNA, 
+       make_FASTA_DNA_w_splits,
+       get_test_set_for_flux,
+       get_train_fold_for_flux,
+       fasta_reshape_for_flux!,
+       fasta_reshape_to_orig!
+
 
 include("constants.jl")
 include("helpers.jl")
